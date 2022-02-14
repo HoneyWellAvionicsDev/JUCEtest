5b668d15676958fb6264534a0ec1cca822837507
Haiden Campos <iambob661@gmail.com>
2022-02-13 18:42:51 -0600

    Window
    Changed window size

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -3,7 +3,7 @@
 //==============================================================================
 MainComponent::MainComponent()
 {
-    setSize (600, 400);
+    setSize (650, 450);
 }
 
 MainComponent::~MainComponent()
